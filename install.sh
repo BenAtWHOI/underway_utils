5be44bc4db9900f816ea66af480a0a225a58e4be
BenAtWHOI <benjamin.foley@whoi.edu>
2024-07-30 12:44:29 -0400

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+# Uninstall previous OpenRVDAS installation if exists
+# (does not remove rvdas user)
+./uninstall_openrvdas.sh
+
+
+# Download and unpack an OpenRVDAS release
+INSTALL_ROOT="/opt/"
+cd $INSTALL_ROOT
+
+RELEASE_TAG="v1.10.2"
+apt-get install git
+git clone https://github.com/OceanDataTools/openrvdas/tags
+git checkout tags/${RELEASE_TAG}
+
+
+# Run its installer script
+./utils/install_openrvdas.sh
+
+    
+# With the virtualenv activated, pip install a release tag of your script from GitHub
+VENV_PATH=$INSTALL_ROOT/openrvdas/venv
+source $VENV_PATH/bin/activate
+pip install https://github.com/BenAtWHOI/underway_rvdas
