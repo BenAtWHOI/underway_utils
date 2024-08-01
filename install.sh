7a835e9f970a510ac04c92caa525deb53cc79526
BenAtWHOI <benjamin.foley@whoi.edu>
2024-08-01 11:00:39 -0400

    Removed whoi implementation version tag for testing

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ cd $INSTALL_ROOT/openrvdas/utils && ./install_openrvdas.sh
 # With the virtualenv activated, pip install a release tag of the WHOI implementation
 . $VENV_PATH/bin/activate
 export PYTHONPATH=$INSTALL_ROOT/openrvdas:$INSTALL_ROOT/openrvdas/logger:$PYTHONPATH
-PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas@$WHOI_IMPLEMENTATION_VERSION
+PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas    #@$WHOI_IMPLEMENTATION_VERSION
 echo 'export PYTHONPATH="$INSTALL_ROOT/openrvdas/logger:$INSTALL_ROOT/openrvdas/:$PYTHONPATH"' >> "$VENV_PATH/bin/activate"
 
 # Prompt to activate venv
