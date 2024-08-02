c2612dd64c4cd213f3ee253e9b37fca472e5ff5e
BenAtWHOI <benjamin.foley@whoi.edu>
2024-08-02 10:42:49 -0400

    Reinstated underway_rvdas versioning

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 INSTALL_ROOT="/opt"
 VENV_PATH=$INSTALL_ROOT/openrvdas/venv
 OPENRVDAS_RELEASE_TAG="v1.10.2"
-WHOI_IMPLEMENTATION_RELEASE_TAG="v0.2.2"
+WHOI_IMPLEMENTATION_RELEASE_TAG="v0.2.3"
 
 # Uninstall any existing OpenRVDAS installation (does not remove rvdas user)
 ./uninstall_openrvdas.sh
@@ -14,7 +14,7 @@ cd $INSTALL_ROOT/openrvdas/utils && ./install_openrvdas.sh
 # With the virtualenv activated, pip install a release tag of the WHOI implementation
 . $VENV_PATH/bin/activate
 export PYTHONPATH=$PyTHONPATH:$INSTALL_ROOT/openrvdas/logger
-PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas    #@$WHOI_IMPLEMENTATION_RELEASE_TAG
+PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas@$WHOI_IMPLEMENTATION_RELEASE_TAG
 echo 'export PYTHONPATH="$PYTHONPATH:$INSTALL_ROOT/openrvdas/logger"' >> "$VENV_PATH/bin/activate"
 
 # Prompt to activate venv
