1e51a2d1d0f159e4442979f272b139a21b0477d0
BenAtWHOI <benjamin.foley@whoi.edu>
2024-08-02 10:25:43 -0400

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
-
 INSTALL_ROOT="/opt"
-RELEASE_TAG="v1.10.2"
 VENV_PATH=$INSTALL_ROOT/openrvdas/venv
-WHOI_IMPLEMENTATION_VERSION="v0.2.2"
+OPENRVDAS_RELEASE_TAG="v1.10.2"
+WHOI_IMPLEMENTATION_RELEASE_TAG="v0.2.2"
 
-# Uninstall previous OpenRVDAS installation if exists
-# (does not remove rvdas user)
+# Uninstall any existing OpenRVDAS installation (does not remove rvdas user)
 ./uninstall_openrvdas.sh
 
-# Download and unpack an OpenRVDAS release
+# Download an OpenRVDAS release and run install script
 cd $INSTALL_ROOT && git clone https://github.com/OceanDataTools/openrvdas
-git checkout tags/${RELEASE_TAG}
-
-# Run its installer script
+git checkout tags/${OPENRVDAS_RELEASE_TAG}
 cd $INSTALL_ROOT/openrvdas/utils && ./install_openrvdas.sh
 
 # With the virtualenv activated, pip install a release tag of the WHOI implementation
 . $VENV_PATH/bin/activate
-export PYTHONPATH=$INSTALL_ROOT/openrvdas:$INSTALL_ROOT/openrvdas/logger:$PYTHONPATH
-PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas    #@$WHOI_IMPLEMENTATION_VERSION
-echo 'export PYTHONPATH="$INSTALL_ROOT/openrvdas/logger:$INSTALL_ROOT/openrvdas/:$PYTHONPATH"' >> "$VENV_PATH/bin/activate"
+export PYTHONPATH=$PyTHONPATH:$INSTALL_ROOT/openrvdas/logger
+PYTHONPATH=$PYTHONPATH pip install git+https://github.com/BenAtWHOI/underway_rvdas    #@$WHOI_IMPLEMENTATION_RELEASE_TAG
+echo 'export PYTHONPATH="$PYTHONPATH:$INSTALL_ROOT/openrvdas/logger"' >> "$VENV_PATH/bin/activate"
 
 # Prompt to activate venv
 echo
